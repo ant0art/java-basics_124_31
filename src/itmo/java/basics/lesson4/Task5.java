8a7284b681c9b4d7f4ebfefef4b561853a12a08c
Anton Filatov <antartbox@gmail.com>
2022-11-24 21:12:04 +0300

    Refactor task-code
    Task1
     -  move code to dedicated method getNumbers()
    
    Task2
     -  move code to dedicated methods: getNumbersDiv3and5var1() and
     getNumbersDiv3and5var2()
    
    Task5
     -  move code to dedicated method checkArrayStartEnd()
     -  change StringBuilder to String
    
    Task8
     -  move code to dedicated methods: setArraySize() and
     setArrayNumbers()

--- src/itmo/java/basics/lesson4/Task5.java
+++ src/itmo/java/basics/lesson4/Task5.java
@@ -3,14 +3,24 @@ package itmo.java.basics.lesson4;
 public class Task5 {
     public static void main(String[] args) {
         int[] array = new int[]{3, -3, 7, 4, 5, 4, 3};
-        StringBuilder stringBuilder = new StringBuilder("array = ").append(array[0]);
+        checkArrayStartEnd(array, 3);
+    }
+
+    /**
+     * Check if array starts and ends with equal number
+     *
+     * @param array array to check
+     * @param n     number to check in array
+     */
+    public static void checkArrayStartEnd(int[] array, int n) {
+        String arrayString = "array = " + array[0];
         for (int i = 1; i < array.length; i++) {
-            stringBuilder.append(", ").append(array[i]);
+            arrayString += ", " + array[i];
         }
-        System.out.println(stringBuilder.toString());
+        System.out.println(arrayString);
         if (array.length < 2)
             System.out.println("Добавьте в массив больше чисел");
         else
-            System.out.println((array[0] == 3) && (array[array.length - 1] == 3));
+            System.out.println((array[0] == n) && (array[array.length - 1] == n));
     }
 }
--- src/itmo/java/basics/lesson4/Task8.java
+++ src/itmo/java/basics/lesson4/Task8.java
@@ -6,16 +6,32 @@ import java.util.Scanner;
 public class Task8 {
 
     public static void main(String[] args) {
-        int size;
+        int[] array = new int[setArraySize()];
+        setArrayNumbers(array);
+        System.out.println("Result: " + Arrays.toString(array));
+    }
+
+    /**
+     * @return size of the array
+     */
+    public static int setArraySize() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Array length: ");
         while (!scanner.hasNextInt()) {
             scanner.next();
             System.out.print("Введите число: ");
         }
-        size = scanner.nextInt();
+        int size = scanner.nextInt();
+        return size;
+    }
 
-        int[] array = new int[size];
+    /**
+     * Fill the current array with entered numbers
+     *
+     * @param array array to fill with numbers
+     */
+    public static void setArrayNumbers(int[] array) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Numbers of array:");
         for (int i = 0; i < array.length; i++) {
             while (!scanner.hasNextInt()) {
@@ -24,7 +40,5 @@ public class Task8 {
             }
             array[i] = scanner.nextInt();
         }
-        System.out.println("Result: " + Arrays.toString(array));
-        scanner.close();
     }
 }
